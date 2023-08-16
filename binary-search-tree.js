cfde4de07dd789b0c109279aa78d08ef6db2c53b
HasanMaaraj <123746697+HasanMaaraj@users.noreply.github.com>
2023-08-16 16:38:09 +0300

    Write factory functions

---
+++ binary-search-tree.js
@@ -0,0 +1,11 @@
+const treeFactory = (array) => {
+    let root = null;
+    const nodeFactory = () => {
+        return {
+            leftNode: null,
+            rightNode: null,
+            data: null,
+
+        }
+    }
+}
